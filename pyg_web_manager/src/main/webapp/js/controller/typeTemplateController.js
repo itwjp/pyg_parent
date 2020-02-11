6ada9d661cf50787b9069edc2018ecdf1b2f3392
itXiaoKe <18894571403@163.com>
2020-02-11 11:13:57 +0800

    模板的修改

--- pyg_web_manager/src/main/webapp/js/controller/typeTemplateController.js
+++ pyg_web_manager/src/main/webapp/js/controller/typeTemplateController.js
@@ -27,6 +27,9 @@ app.controller('typeTemplateController', function ($scope, $controller, typeTemp
         typeTemplateService.findOne(id).success(
             function (response) {
                 $scope.entity = response;
+                $scope.entity.brandIds = JSON.parse($scope.entity.brandIds);
+                $scope.entity.specIds = JSON.parse($scope.entity.specIds);
+                $scope.entity.customAttributeItems = JSON.parse($scope.entity.customAttributeItems)
             }
         );
     };
